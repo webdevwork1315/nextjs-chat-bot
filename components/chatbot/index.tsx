1f0778554a721268965fd07dda679968c5b5f8c2
iamsabbirsobhani <sabbirsobhani@gmail.com>
2024-06-20 11:07:58 +0600

    add chat input

--- components/chatbot/index.tsx
+++ components/chatbot/index.tsx
@@ -15,7 +15,34 @@ export default function Chatbot() {
         </div>
         <h1>Chatbot</h1>
       </div>
-      <div className="bg-custom-gray h-[70vh] rounded-md mt-2"></div>
+      <div className="bg-custom-gray h-[60vh] rounded-md mt-2"></div>
+      <div className="relative">
+        <input
+          className="bg-custom-gray h-[9vh] rounded-full px-10 mt-2 w-full outline-none text-lg"
+          type="text"
+          name=""
+          id=""
+          placeholder="Reply to chat"
+        />
+        <button>
+          <svg
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            strokeWidth={1.5}
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19.5716 2.42859L13.5716 19.5714L9.28585 12.7143L2.42871 8.42859L19.5716 2.42859Z"
+              stroke="white"
+              stroke-width="2.05714"
+              stroke-miterlimit="10"
+              stroke-linecap="square"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
