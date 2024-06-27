fa63e2c33cd883f2a682d49a4126eff3ee80e774
iamsabbirsobhani <sabbirsobhani@gmail.com>
2024-06-28 00:04:12 +0600

    modify layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.className}>
-      <body>
+    <html lang="en">
+      <body className={inter.className}>
         <Providers>
           <div>{children}</div>
         </Providers>
